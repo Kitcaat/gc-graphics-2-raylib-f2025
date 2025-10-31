f99899c4aa6797b50415d773c53a004c8bb18a16
Suzanna Lezhenko <200556184@student.georgianc.on.ca>
2025-10-31 10:00:10 -0400

    For loop example

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,11 +1,26 @@
 #include "raylib.h"
 #include "raymath.h"
+#include <iostream>
 
 int main()
 {
+    int health = 100;
+    health++;
+
+    // Counts from 0 to 4
+    for (int number = 0; number < 5; number++)
+    {
+        std::cout << number << std::endl;
+    }
+    
+    // Counts from 1 to 5
+    for (int number = 1; number <= 5; number++)
+    {
+        std::cout << number << std::endl;
+    }
+
     InitWindow(800, 800, "Game");
     SetTargetFPS(60);
-
     while (!WindowShouldClose())
     {
         BeginDrawing();
